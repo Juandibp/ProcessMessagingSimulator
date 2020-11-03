315887e8c149124dc565865b820d19f50c347f0e
Juandi <juandibp@gmail.com>
2020-11-03 10:53:26 -0600

    config class

---
+++ config.py
@@ -0,0 +1,7 @@
+class config:
+  def __init__(self, psend, precieve, pdir, pformat, pqueues):
+    self.send = psend
+    self.recieve = precieve
+    self.dir = pdir
+    self.format = pformat
+    self. queues = pqueues
