dfa8538eb92a4d856e9309c99acd663f86bd5dff
Juandi <juandibp@gmail.com>
2020-11-03 11:24:07 -0600

    minor bug fix

--- concreteProcessCreator.py
+++ concreteProcessCreator.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 from processCreator import *
 
-class concreteProcessCreator(Creator):
+
+class concreteProcessCreator(processCreator):
     """
     Note that the signature of the method still uses the abstract product type,
     even though the concrete product is actually returned from the method. This
@@ -9,4 +12,3 @@ class concreteProcessCreator(Creator):
     def factory_method(self) -> process:
         return concreteProcessCreator()
 
-    
