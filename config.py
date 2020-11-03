67386c37fdfcc766419d5de13d2a2b563596f977
Juandi <juandibp@gmail.com>
2020-11-03 11:39:33 -0600

    made config a singleton

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
 class config:
+  class __config:
+    def __init__(self, psend, precieve, pdir, pformat, pqueues):
+      self.send = psend #sync
+      self.recieve = precieve #sync
+      self.dir = pdir #lista de parametros
+      self.format = pformat
+      self. queues = pqueues
+  instance = None
   def __init__(self, psend, precieve, pdir, pformat, pqueues):
-    self.send = psend #sync
-    self.recieve = precieve #sync
-    self.dir = pdir #lista de parametros
-    self.format = pformat
-    self. queues = pqueues
+    if not config.instance:
+      config. instance = config.__config(psend, precieve, pdir, pformat, pqueues)
+    else:
+      config.instance.send = psend #sync
+      config.instance.recieve = precieve #sync
+      config.instance.dir = pdir #lista de parametros
+      config.instance.format = pformat
+      config.instance. queues = pqueues
+  def __getattribute__(self, name):
+    return getattr(self.instance, name)
